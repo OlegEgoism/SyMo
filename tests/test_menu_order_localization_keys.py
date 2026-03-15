05136df07b242a3535994370ef9c8a645687e793
oleg <olegpustovalov220@gmail.com>
2026-03-15 19:28:08 +0300

    Рабочий проект. В прод смело.

--- tests/test_menu_order_localization_keys.py
+++ tests/test_menu_order_localization_keys.py
@@ -4,12 +4,10 @@ from app_core.language import LANGUAGES
 def test_menu_order_keys_exist_for_all_languages():
     required = {
         'menu_order_title',
-        'menu_order_hint',
         'display_section',
         'logging_section',
         'notification_section',
         'graph_history_minutes',
-        'graph_history_hint',
     }
     for lang_code, lang_map in LANGUAGES.items():
         missing = required - set(lang_map.keys())
